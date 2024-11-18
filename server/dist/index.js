81086fcd7d7e7d0b3eba95063c6dbb3b39e388cd
banon <j.banon01@outlook.fr>
2024-11-18 17:02:46 +0100

    install : apollo server init

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,22 +1,18 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-
 const typeDefs = `#graphql
   type Query {
     hello: String
   }
 `;
-
 const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-  },
+    Query: {
+        hello: () => 'Hello world!',
+    },
 };
-
 const server = new ApolloServer({ typeDefs, resolvers });
-
 startStandaloneServer(server, {
-  listen: { port: 4000 },
+    listen: { port: 4000 },
 }).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+    console.log(`🚀 Server ready at ${url}`);
 });
